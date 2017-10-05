e08145ebedd17930984e8baff48a8ca71b40d730
feng <fenghonghei@gmail.com>
2017-10-05 22:41:13 +0800

    添加抢单后的震动提示

--- app/src/main/java/com/example/feng/weiyi/WeiYiService.kt
+++ app/src/main/java/com/example/feng/weiyi/WeiYiService.kt
@@ -2,7 +2,9 @@ package com.example.feng.weiyi
 
 import android.accessibilityservice.AccessibilityService
 import android.annotation.TargetApi
+import android.content.Context
 import android.os.Build
+import android.os.Vibrator
 import android.support.annotation.RequiresApi
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
@@ -29,13 +31,17 @@ class WeiYiService : AccessibilityService() {
         when (accessibilityEvent?.eventType) {
             AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED,
             AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED -> {
-                if (accessibilityEvent.className == "com.greenline.yihuantong.message.gpconsultation.GPConsultationListActivity") {
+                if (accessibilityEvent.className == "com.greenline.yihuantong.consult.specialclinic.SpecialConsultDetailActivity") {
+                    Log.e("milan", "consult")
+                } else if (accessibilityEvent.className == "com.greenline.yihuantong.message.gpconsultation.GPConsultationListActivity") {
                     val nextNodeInfos = rootInActiveWindow?.findAccessibilityNodeInfosByViewId("com.greenline.yihuantong:id/special_layout")
                     nextNodeInfos!![0].performAction(AccessibilityNodeInfo.ACTION_CLICK)
                 } else if (accessibilityEvent.className == "com.greenline.yihuantong.consult.specialclinic.SpecialClinicListActivity") {
                     val handleNodeInfos = rootInActiveWindow?.findAccessibilityNodeInfosByViewId("com.greenline.yihuantong:id/tv_accept_order")
                     if (handleNodeInfos!!.size > 0) {
                         handleNodeInfos[0].performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                        val service = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                        service.vibrate(400)
                     } else {
                         val backNodeInfos = rootInActiveWindow?.findAccessibilityNodeInfosByViewId("com.greenline.yihuantong:id/actionbar_home_btn")
                         backNodeInfos!![0].performAction(AccessibilityNodeInfo.ACTION_CLICK)
